07f40deeceba24fa160d34a57c4ba6ec487e01e1
Anton Antonov <anton525anton@gmail.com>
2023-04-21 18:18:18 +0200

    init:C_sem_1 with .gitignore

---
+++ C_sem_1/Program.cs
@@ -0,0 +1,68 @@
+﻿
+//Task 2.
+
+/*
+Console.WriteLine ("Input number_1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Input number_2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine ());
+
+if (number_1 > number_2)
+{
+    Console.WriteLine ($"max = {number_1}");
+}
+else
+{
+    Console.WriteLine ($"max = {number_2}");
+}
+*/
+
+
+//Task 4.
+
+/*
+Console.WriteLine ("Input number_1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Input number_2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Input number_3: ");
+int number_3 = Convert.ToInt32(Console.ReadLine ());
+
+int max = (number_1 >= number_2) ? number_1 : number_2;
+max = (max >= number_3) ? max : number_3;
+Console.WriteLine ($"max = {max}");
+*/
+
+
+
+
+//Task 6.
+
+/*
+Console.WriteLine ("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine ());
+
+string result = (number % 2 == 0) ? "Even" : "Odd";
+Console.WriteLine(result);
+*/
+
+
+
+
+//Task 8.
+
+/*
+Console.WriteLine ("Input your number: ");
+int number_N = Convert.ToInt32(Console.ReadLine ());
+
+int current_i = 0;
+
+while (current_i <= number_N)
+{
+    if (current_i % 2 == 0)
+    {
+        Console.Write ($"{current_i}, ");
+    }
+    current_i++;
+}
+*/
